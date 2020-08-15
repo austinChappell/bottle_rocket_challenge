6cbe48414e870c24b3d42bcccfc92aefdcb89c6c
Austin <austin.w.chappell@gmail.com>
2020-08-15 16:37:56 -0500

    clean up icon button size

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -2,20 +2,9 @@
 import { CSSProperties } from 'react';
 
 // Local Dependencies
-import { mediaQueries } from './breakpoints';
-
 export const navButtonHeight = 32;
 
 export const navButtonHeightStylesWithMediaQueries: CSSProperties = {
   height: navButtonHeight,
   width: navButtonHeight,
-
-  [`${mediaQueries.isTabletMin}`]: {
-    height: navButtonHeight * 1.2,
-    width: navButtonHeight * 1.2,
-  },
-  [`${mediaQueries.isDesktopMin}`]: {
-    height: navButtonHeight * 1.4,
-    width: navButtonHeight * 1.4,
-  },
 };
